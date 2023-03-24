61d1730ac686ab5a65bbdc6550ed449f48c0b8a7
adhu2018 <41824074+adhu2018@users.noreply.github.com>
2023-03-24 09:54:26 +0800

    up to python 3.11

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-python -m venv venv&cd venv\Scripts&activate&cd ../..&pip install -r requirements.txt&pip install pyinstaller&pyinstaller -F xapkInstaller.py -i NONE --clean
+@echo off
+chcp 65001
+del /q .\venv\*.*
+rd /s /q .\venv
+py -3 -m venv venv&cd venv\Scripts&activate&cd ../..&pip install -U pip&pip install -r requirements.txt&pip install pyinstaller&pyinstaller -F xapkInstaller.py -i NONE --clean
